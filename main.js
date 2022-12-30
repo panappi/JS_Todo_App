b51fc34885ab1c38e4253274b346e1715fb2a446
panappi <119737780+panappi@users.noreply.github.com>
2022-12-30 15:55:32 +0900

    feat: タスク情報(ボタン)の追加
    状態(作業中)、削除の2つのボタンを追加

--- main.js
+++ main.js
@@ -6,32 +6,59 @@ const text = document.getElementById("text"), // 入力されたテキスト
 const addButtonClick = (el) => {
   return (task.innerText = el.value);
 };
+let numId = 0;
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  addButtonClick(text);
+  // addButtonClick(text);
   console.log(text.value);
 
-  const taskInfo = [
-    {
-      id: "0",
-      name: text.value,
-      status: "作業中",
-      dlt: "削除",
-    },
-  ];
+  const trTag = document.createElement("tr");
+  taskList.appendChild(trTag);
+  // IDをtdに追加
+  const tdTagId = document.createElement("td");
+  tdTagId.textContent = numId++;
+  // console.log(tdTagId);
+  trTag.appendChild(tdTagId);
+  // 名前をtdに追加
+  const tdTagName = document.createElement("td");
+  tdTagName.textContent = text.value;
+  // console.log(tdTagName);
+  trTag.appendChild(tdTagName);
+  // 作業中ボタンをtdに追加
+  const tdTagStatus = document.createElement("td");
+  const buttonStatus = document.createElement("button");
+  buttonStatus.innerHTML = "作業中";
+  // console.log(buttonStatus);
+  tdTagStatus.appendChild(buttonStatus);
+  trTag.appendChild(tdTagStatus);
+  // 削除ボタンをtdに追加
+  const tdTagDelete = document.createElement("td");
+  const buttonDelete = document.createElement("button");
+  buttonDelete.innerHTML = "削除";
+  tdTagDelete.appendChild(buttonDelete);
+  trTag.appendChild(tdTagDelete);
 
-  taskInfo.forEach((todo) => {
-    // 配列の中のオブジェクトの数だけ処理を繰り返す
-    const tr = document.createElement("tr");
-    taskList.appendChild(tr);
-    const objArray = Object.entries(todo); // オブジェクトを配列に変換
-    console.log(objArray);
-    objArray.forEach((arr) => {
-      const td = document.createElement("td");
-      td.textContent = arr[1]; // arr[1]はvalueの部分
-      tr.appendChild(td);
-    });
-  });
+  // const taskInfo = [
+  //   {
+  //     id: "0",
+  //     name: text.value,
+  //     status: "作業中",
+  //     dlt: "削除",
+  //   },
+  // ];
+
+  // taskInfo.forEach((todo) => {
+  //   // 配列の中のオブジェクトの数だけ処理を繰り返す
+  //   const tr = document.createElement("tr");
+  //   taskList.appendChild(tr);
+  //   const objArray = Object.entries(todo); // オブジェクトを配列に変換
+  //   console.log(objArray);
+  //   objArray.forEach((arr) => {
+  //     const td = document.createElement("td");
+  //     td.textContent = arr[1]; // arr[1]はvalueの部分
+  //     tr.appendChild(td);
+  //   });
+  // });
 });
 
 //////////
