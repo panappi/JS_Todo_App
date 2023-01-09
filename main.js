052fe217c514cfa2dd9db1c669a5ddb94e4bac22
panappi <119737780+panappi@users.noreply.github.com>
2023-01-09 22:41:24 +0900

    Ver. 1.0 (#7)
    * first commit
    
    * feat: 新規htmlファイル作成
    
    * feat: タスク追加フォームの追加
    
    * feat: タスク情報の追加
    
    ## タスク情報
    ID、状態、削除
    ※状態と削除はボタンにする予定だが未実装
    
    * feat: タスク情報(ボタン)の追加
    
    状態(作業中)、削除の2つのボタンを追加
    
    * feat: タスクの状態ボタンの機能追加
    
    * feat: タスク削除機能の追加
    
    削除ボタンを押すと対象タスクが削除されるよう機能追加
    残っているタスクのIDが変わるよう実装予定
    
    * feat: タスク削除後のID変更機能の追加
    
    残っているタスクのIDを振り直す機能を追加
    
    * feat: フォームリセット機能追加
    
    タスク追加後にフォーム内のテキストをリセットする機能を追加
    
    * feat: タスク絞り込み機能の追加
    
    * refactor: 修正前のコード削除
    
    * refactor: 不要コード削除
    
    * refactor: コード修正、説明文追加
    
    各関数に説明文をコメントアウトで追加
    displayTask関数のコード修正
    filterTask関数のコード修正
    filterCheck関数の削除
    
    * refactor: コード修正、説明文追加

---
+++ main.js
@@ -0,0 +1,139 @@
+const ALL = "すべて";
+const DOING = "作業中";
+const DONE = "完了";
+const REMOVE = "削除";
+
+const radioForm = document.getElementById("radioForm");
+const textForm = document.getElementById("textForm");
+const text = document.getElementById("text"); // 入力されたテキスト
+const addButton = document.getElementById("addButton");
+const taskList = document.getElementById("taskList");
+
+let taskArray = [
+  // {
+  //   id: 0,
+  //   name: "ほげ",
+  //   status: DOING,
+  //   delete: REMOVE,
+  // },
+  // {
+  //   id: 1,
+  //   name: "ほげ",
+  //   status: DOING,
+  //   delete: REMOVE,
+  // },
+  // {
+  //   id: 2,
+  //   name: "ほげ",
+  //   status: DOING,
+  //   delete: REMOVE,
+  // },
+];
+
+// タスクを配列に追加する
+const addTaskArray = (taskArray) => {
+  taskArray.push({
+    id: taskArray.length,
+    name: text.value,
+    status: DOING,
+    delete: REMOVE,
+  });
+};
+
+// タスクの状態をトグルで入れ替える
+const toggleStatus = (id) => {
+  taskArray = taskArray.map((task) => {
+    if (task.id === id) {
+      return {
+        ...task,
+        status: task.status === DOING ? DONE : DOING,
+      };
+    }
+    return task;
+  });
+  displayTaskArray(taskArray);
+};
+
+// タスクを配列から削除する
+const deleteTask = (id) => {
+  taskArray = taskArray
+    .filter((task) => task.id !== id)
+    .map((task, index) => {
+      return {
+        ...task,
+        id: index,
+      };
+    });
+  displayTaskArray(taskArray);
+};
+
+// タスクをhtmlに表示する(tbodyに追加する)
+const displayTaskArray = (taskArray) => {
+  taskList.innerHTML = ""; // tbodyを初期化
+
+  taskArray.map((task) => {
+    // タスクのDOMを生成
+    const tr = document.createElement("tr");
+    const tdId = document.createElement("td");
+    const tdName = document.createElement("td");
+    const tdStatus = document.createElement("td");
+    const tdDelete = document.createElement("td");
+    const buttonStatus = document.createElement("button");
+    const buttonDelete = document.createElement("button");
+
+    taskList.appendChild(tr);
+    // タスクのID
+    tdId.textContent = task.id;
+    tr.appendChild(tdId);
+    // タスクの名前
+    tdName.textContent = task.name;
+    tr.appendChild(tdName);
+    // 状態ボタン
+    buttonStatus.textContent = task.status;
+    tdStatus.appendChild(buttonStatus);
+    tr.appendChild(tdStatus);
+    // 削除ボタン
+    buttonDelete.textContent = REMOVE;
+    tdDelete.appendChild(buttonDelete);
+    tr.appendChild(tdDelete);
+
+    // 状態ボタンが押されたときの処理
+    buttonStatus.addEventListener("click", () => {
+      toggleStatus(task.id);
+    });
+    // 削除ボタンが押されたときの処理
+    buttonDelete.addEventListener("click", () => {
+      deleteTask(task.id);
+    });
+  });
+};
+
+displayTaskArray(taskArray);
+
+// タスク入力フォームのテキストをリセットする
+const formReset = (textForm) => textForm.reset();
+
+// ラジオボタンに合わせてタスクの表示を変更する
+const filterTask = () => {
+  // 選択されているラジオボタンのvalueを取得する
+  const selectedRadio = radioForm.status.value;
+  const filterTaskArray = taskArray.filter(
+    (task) => task.status == selectedRadio
+  );
+  selectedRadio === ALL
+    ? displayTaskArray(taskArray)
+    : displayTaskArray(filterTaskArray);
+};
+
+// 追加ボタンが押されたときの処理
+textForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addTaskArray(taskArray);
+  displayTaskArray(taskArray);
+  formReset(textForm);
+});
+
+// ラジオボタンが押されたときの処理
+radioForm.addEventListener("click", () => {
+  filterTask();
+});
